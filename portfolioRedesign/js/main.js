1babfb8d1912c9b27a0792d441af34db49a8a00d
Elvin Samuel <elvinsamuel1@gmail.com>
2018-11-03 16:32:18 +0000

    Got the form up and running (not attached to PHP yet). Javascript running as well. Next step, adding the actual content

---
+++ portfolioRedesign/js/main.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("formNav").style.height = "100%";
+    document.getElementById("myNav").style.backgroundColor = "#333";
+}
+
+function closeForm() {
+    document.getElementById("formNav").style.height = "0%";
+    document.getElementById("myNav").style.backgroundColor = "#00688B"
+}
